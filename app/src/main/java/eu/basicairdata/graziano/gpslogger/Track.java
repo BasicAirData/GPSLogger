d203967ab3e3141f5fe6caf4fc8a7c1902bb5a6c
GrazianoCapelli <air@shweb.it>
2019-08-29 18:38:23 +0200

    Fix Average Speed calculation

--- app/src/main/java/eu/basicairdata/graziano/gpslogger/Track.java
+++ app/src/main/java/eu/basicairdata/graziano/gpslogger/Track.java
@@ -257,8 +257,8 @@ public class Track {
         // --------------------------------------------------------------------------------- Speeds
 
         if ((End_Speed != NOT_AVAILABLE) && (End_Speed > SpeedMax)) SpeedMax = End_Speed;
-        if (Duration > 0) SpeedAverage = Distance / ((float) Duration / 1000);
-        if (Duration_Moving > 0) SpeedAverageMoving = Distance / ((float) Duration_Moving / 1000);
+        if (Duration > 0) SpeedAverage = (Distance + DistanceInProgress) / (((float) Duration) / 1000f);
+        if (Duration_Moving > 0) SpeedAverageMoving = (Distance + DistanceInProgress) / (((float) Duration_Moving) / 1000f);
         NumberOfLocations++;
     }
 
