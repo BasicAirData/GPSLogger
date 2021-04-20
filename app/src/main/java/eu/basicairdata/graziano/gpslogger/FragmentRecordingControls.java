dfd28cf04a2b53858f03bb5c3dd184cf413f7520
GrazianoCapelli <air@shweb.it>
2021-04-20 20:40:15 +0200

    Issue #88 - Disable the Stop button when the track is empty

--- app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentRecordingControls.java
+++ app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentRecordingControls.java
@@ -232,6 +232,10 @@ public class FragmentRecordingControls extends Fragment{
                     setTextViewDrawableColor(TVLockButton.getCompoundDrawables()[1],
                             getResources().getColor(isLck ? R.color.textColorRecControlPrimary_Active : R.color.textColorRecControlPrimary));
                 }
+                if (TVLockButton != null) {
+                    TVStopButton.setAlpha(track.getNumberOfLocations() + track.getNumberOfPlacemarks() > 0 ? 1.0f : 0.4f);
+                    TVStopButton.setClickable(track.getNumberOfLocations() + track.getNumberOfPlacemarks() > 0);
+                }
             }
         }
     }
