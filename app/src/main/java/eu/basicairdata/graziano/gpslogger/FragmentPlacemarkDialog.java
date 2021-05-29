cad9ecf51e653e237b411c67f5ae21ce92ad77b9
GrazianoCapelli <air@shweb.it>
2021-05-29 11:49:32 +0200

    Move the "Cancel" button in the default left position

--- app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentPlacemarkDialog.java
+++ app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentPlacemarkDialog.java
@@ -83,7 +83,7 @@ public class FragmentPlacemarkDialog extends DialogFragment {
                         }
                     }
                 })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                .setNeutralButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                     }
