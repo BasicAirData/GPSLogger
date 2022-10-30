b01072e3ca39a95fad54747a424037fb1c7ec1a0
GrazianoCapelli <air@shweb.it>
2022-10-30 22:02:13 +0100

    #152 - Moved the Activity Type definitions into the Track class

--- app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentActivityTypeDialog.java
+++ app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentActivityTypeDialog.java
@@ -59,16 +59,16 @@ public class FragmentActivityTypeDialog extends DialogFragment {
         }
     }
 
-   /**
-    * The class that defines an activity (represented by an icon)
-    */
-    private static class ActivityType {
+    /**
+     * The class that defines the activities
+     */
+    public static class ActivityType {
         public int value;
         public int drawableId;
 
-        ActivityType(int value, int drawableId) {
+        ActivityType(int value) {
             this.value = value;
-            this.drawableId = drawableId;
+            this.drawableId = Track.ACTIVITY_DRAWABLE_RESOURCE[value];
         }
     }
 
@@ -90,78 +90,78 @@ public class FragmentActivityTypeDialog extends DialogFragment {
 
         // Fitness
         ActivityCategory acFitness = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_fitness));
-        acFitness.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_walk_24dp));
-        acFitness.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_hiking_24));
-        acFitness.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_nordic_walking_24));
-        acFitness.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_run_24dp));
+        acFitness.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_WALK));
+        acFitness.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_HIKING));
+        acFitness.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_NORDICWALKING));
+        acFitness.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_RUN));
         activityCategories.add(acFitness);
 
         // Water
         ActivityCategory acWatersports = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_water));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_pool_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_scuba_diving_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_rowing_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_kayaking_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_surfing_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_kitesurfing_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_sailing_24));
-        acWatersports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_directions_boat_24));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SWIMMING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SCUBADIVING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_ROWING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_KAYAKING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SURFING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_KITESURFING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SAILING));
+        acWatersports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_BOAT));
         activityCategories.add(acWatersports);
 
         // Snow and Ice
         ActivityCategory acSnowIce = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_snow));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_downhill_skiing_24));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_snowboarding_24));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_sledding_24));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_snowmobile_24));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_snowshoeing_24));
-        acSnowIce.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_ice_skating_24));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_DOWNHILLSKIING));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SNOWBOARDING));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SLEDDING));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SNOWMOBILE));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SNOWSHOEING));
+        acSnowIce.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_ICESKATING));
         activityCategories.add(acSnowIce);
 
         // Air
         ActivityCategory acAir = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_air));
-        acAir.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_flight_24dp));
-        acAir.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_rocket_24));
-        acAir.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_paragliding_24));
+        acAir.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_FLIGHT));
+        acAir.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_ROCKET));
+        acAir.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_PARAGLIDING));
         activityCategories.add(acAir);
 
         // Wheel
         ActivityCategory acWheel = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_wheel));
-        acWheel.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_bike_24dp));
-        acWheel.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_skateboarding_24));
-        acWheel.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_roller_skating_24));
-        acWheel.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_accessible_24));
+        acWheel.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_BICYCLE));
+        acWheel.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SKATEBOARDING));
+        acWheel.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_ROLLERSKATING));
+        acWheel.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_WHEELCHAIR));
         activityCategories.add(acWheel);
 
         // Wheel Mobility
         ActivityCategory acMobility = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_mobility));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_electric_scooter_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_moped_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_sports_motorsports_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_car_24dp));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_local_shipping_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_directions_bus_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_tram_24));
-        acMobility.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_train_24));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_ELECTRICSCOOTER));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_MOPED));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_MOTORCYCLE));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_CAR));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_TRUCK));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_BUS));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_TRAM));
+        acMobility.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_TRAIN));
         activityCategories.add(acMobility);
 
         // Other
         ActivityCategory acOther = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_other));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_place_24dp));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_mountain_24dp));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_location_city_24));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_work_24));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_forest_24));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_camera_24));
-        acOther.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_search_24));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_STEADY));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_MOUNTAIN));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_CITY));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_FOREST));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_WORK));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_PHOTOGRAPHY));
+        acOther.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_RESEARCH));
         activityCategories.add(acOther);
 
         // Other
         ActivityCategory acOtherSports = new ActivityCategory(view.findViewById(R.id.tracktype_main_linearlayout_cat_other_sports));
-        acOtherSports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_sports_soccer_24));
-        acOtherSports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_golf_24));
-        acOtherSports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_pets_24));
-        acOtherSports.activityTypeList.add(new ActivityType(0, R.drawable.ic_tracktype_map_24));
+        acOtherSports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_SOCCER));
+        acOtherSports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_GOLF));
+        acOtherSports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_PETS));
+        acOtherSports.activityTypeList.add(new ActivityType(Track.TRACK_TYPE_MAP));
         activityCategories.add(acOtherSports);
 
         // Method that works with Android 4.X
