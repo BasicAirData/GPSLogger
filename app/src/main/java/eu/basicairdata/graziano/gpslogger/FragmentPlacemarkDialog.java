fde1ceadc63e38f209404ba468955252eaf456e7
GrazianoCapelli <air@shweb.it>
2026-04-23 23:49:34 +0200

    #247 - Implement preferences,listener and pin icon

--- app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentPlacemarkDialog.java
+++ app/src/main/java/eu/basicairdata/graziano/gpslogger/FragmentPlacemarkDialog.java
@@ -24,17 +24,26 @@ package eu.basicairdata.graziano.gpslogger;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.appcompat.app.AlertDialog;
+import androidx.preference.PreferenceManager;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -46,15 +55,25 @@ import java.util.ArrayList;
  */
 public class FragmentPlacemarkDialog extends DialogFragment {
 
-    static class Tip {
-        TextView textviewTip;
+    static class TipUILine {
+        TextView textviewTipUILine;
+        LinearLayout linearLayoutTipUILine;
+        ImageView imageViewTipUILine;
 
-        public Tip(View view, int i) {
-            textviewTip = view.findViewWithTag("placemark_tv_tip" + i);
+        public TipUILine(View view, int i) {
+             textviewTipUILine = view.findViewWithTag("placemark_tv_tip" + i);
+             linearLayoutTipUILine = view.findViewWithTag("placemark_ll_tip" + i);
+             imageViewTipUILine = view.findViewWithTag("placemark_pin_tip" + i);
         }
     }
 
-    ArrayList<Tip> tipArrayList = new ArrayList<>();
+    ArrayList<String> tipArrayList = new ArrayList<>();
+    ArrayList<TipUILine> uiLineArrayList = new ArrayList<>();
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    ImageView imageViewPin;
 
     EditText etDescription;
 
@@ -69,7 +88,41 @@ public class FragmentPlacemarkDialog extends DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         final View view = inflater.inflate(R.layout.fragment_placemark_dialog, null);
 
+        imageViewPin = view.findViewById(R.id.placemark_pin);
+        imageViewPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tipArrayList.size() < 5) tipArrayList.add(new String(etDescription.getText().toString().trim()));
+                writeTipPrefs();
+                initTipUIList(view);
+                setPinButtonState();
+            }
+        });
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        editor = preferences.edit();
+
         etDescription = view.findViewById(R.id.placemark_description);
+        etDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.w("myApp", "[#] FragmentPlacemarkDialog.java - onEditorAction");
+                setPinButtonState();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                // TODO Auto-generated method stub
+            }
+        });
+
         etDescription.postDelayed(new Runnable()
         {
             public void run()
@@ -82,18 +135,15 @@ public class FragmentPlacemarkDialog extends DialogFragment {
             }
         }, 200);
 
-        // Set tip list
+        // Set tip string list
         for (int i = 0; i < 5; i++) {
-            Tip tip = new Tip(view, i);
-            tip.textviewTip.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    etDescription.setText(tip.textviewTip.getText());
-                    etDescription.setSelection(etDescription.getText().length());
-                }
-            });
-            tipArrayList.add(tip);
+            String stip = preferences.getString("prefAnnotationDescTip" + i, "");
+            Log.w("myApp", "[#] FragmentPlacemarkDialog.java - Tip " + i + " = " + stip);
+            if (!stip.isEmpty()) tipArrayList.add(stip);
         }
+
+        initTipUIList(view);
+        setPinButtonState();
 
         createPlacemarkAlert.setView(view)
                 .setPositiveButton(R.string.dlg_button_add, new DialogInterface.OnClickListener() {
@@ -117,9 +167,84 @@ public class FragmentPlacemarkDialog extends DialogFragment {
         return createPlacemarkAlert.create();
     }
 
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+
+    public void writeTipPrefs() {
+        for (int i = 0; i < 5; i++) {
+            String str;
+            try {
+                str = tipArrayList.get(i);
+                // The tip string exists
+                editor.putString("prefAnnotationDescTip" + i, str);
+
+            } catch (Exception e) {
+                // The tip string doesn't exist
+                // Deletes preferences
+                if (preferences.contains("prefAnnotationDescTip" + i)) {
+                    editor.remove("prefAnnotationDescTip" + i);
+                }
+            }
+        }
+        editor.commit();
+    }
+
+
+    public void setPinButtonState() {
+        if ((tipArrayList.size() >= 5) || (etDescription.getText().toString().trim().isEmpty())) {
+            // Tip list full
+            imageViewPin.setEnabled(false);
+            imageViewPin.setAlpha(0.4f);
+            return;
+        } else {
+            // Tip list has free spaces
+            for (int i = 0; i < tipArrayList.size(); i++) {
+                if (etDescription.getText().toString().trim().equals(tipArrayList.get(i))) {
+                    // Tip already present
+                    imageViewPin.setEnabled(false);
+                    imageViewPin.setAlpha(0.4f);
+                    return;
+                }
+            }
+        }
+        imageViewPin.setEnabled(true);
+        imageViewPin.setAlpha(1.0f);
+    }
+
+
+    public void initTipUIList(View view) {
+        // Set TipUILine list
+        for (int i = 0; i < 5; i++) {
+            TipUILine tip = new TipUILine(view, i);
+            if (i < tipArrayList.size()) {
+                tip.textviewTipUILine.setText(tipArrayList.get(i));
+                tip.linearLayoutTipUILine.setVisibility(View.VISIBLE);
+            } else {
+                tip.linearLayoutTipUILine.setVisibility(View.GONE);
+            }
+            tip.textviewTipUILine.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    etDescription.setText(tip.textviewTipUILine.getText());
+                    etDescription.setSelection(etDescription.getText().length());
+                }
+            });
+            int finalI = i;
+            tip.imageViewTipUILine.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    tipArrayList.remove(finalI);
+                    writeTipPrefs();
+                    initTipUIList(view);
+                    setPinButtonState();
+                }
+            });
+            uiLineArrayList.add(tip);
+        }
     }
 }
